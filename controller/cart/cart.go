dc0cae3978ac5b1827865e6f92022959302457f7
捕影 <qiusheng.fqs@antfin.com>
2018-05-06 17:21:05 +0800

    revise cart list bug

--- controller/cart/cart.go
+++ controller/cart/cart.go
@@ -1,11 +1,13 @@
 package cart
 
 import (
-	"strconv"
+	"encoding/json"
+	"fmt"
 	"gopkg.in/kataras/iris.v6"
+	"strconv"
 	"wemall/config"
-	"wemall/model"
 	"wemall/controller/common"
+	"wemall/model"
 )
 
 // Create 购物车中添加商品
@@ -30,7 +32,7 @@ func Create(ctx *iris.Context) {
 	}
 
 	session := ctx.Session()
-	openID  := session.GetString("weAppOpenID")
+	openID := session.GetString("weAppOpenID")
 
 	if openID == "" {
 		SendErrJSON("登录超时", ctx)
@@ -43,22 +45,30 @@ func Create(ctx *iris.Context) {
 		return
 	}
 	ctx.JSON(iris.StatusOK, iris.Map{
-		"errNo" : model.ErrorCode.SUCCESS,
-		"msg"   : "success",
-		"data"  : iris.Map{
+		"errNo": model.ErrorCode.SUCCESS,
+		"msg":   "success",
+		"data": iris.Map{
 			"id": cart.ID,
 		},
 	})
 	return
 }
 
+//add struct Cart
+type CartProduct struct {
+	Cart    model.Cart    `json:"cart"`
+	Product model.Product `json:"product"`
+}
+
 // List 商品列表
 func List(ctx *iris.Context) {
 	SendErrJSON := common.SendErrJSON
 	var carts []model.Cart
+	var product model.Product
+	var cartProducts []CartProduct
 
 	session := ctx.Session()
-	openID  := session.GetString("weAppOpenID")
+	openID := session.GetString("weAppOpenID")
 
 	if openID == "" {
 		SendErrJSON("登录超时", ctx)
@@ -66,7 +76,7 @@ func List(ctx *iris.Context) {
 	}
 
 	pageNo, err := strconv.Atoi(ctx.FormValue("pageNo"))
- 
+
 	if err != nil || pageNo < 1 {
 		pageNo = 1
 	}
@@ -78,7 +88,7 @@ func List(ctx *iris.Context) {
 	if ctx.FormValue("asc") == "1" {
 		orderStr += " asc"
 	} else {
-		orderStr += " desc"	
+		orderStr += " desc"
 	}
 
 	pageSize := config.ServerConfig.PageSize
@@ -89,21 +99,82 @@ func List(ctx *iris.Context) {
 		return
 	}
 
-//	for i := 0; i < len(products); i++ {
-//		err := model.DB.First(&products[i].Image, products[i].ImageID).Error
-//		if err != nil {
-//			fmt.Println(err.Error())
-//			SendErrJSON("error", ctx)
-//			return
-//		}	
-//	}
+	for _, cart := range carts {
+		err := model.DB.First(&product, cart.ProductID).Error
+		if err != nil {
+			fmt.Println(err.Error())
+			SendErrJSON("error", ctx)
+			return
+		}
+
+		if model.DB.First(&product.Image, product.ImageID).Error != nil {
+			product.Image = model.Image{}
+		}
+
+		var imagesSQL []uint
+		if err := json.Unmarshal([]byte(product.ImageIDs), &imagesSQL); err == nil {
+			var images []model.Image
+			if model.DB.Where("id in (?)", imagesSQL).Find(&images).Error != nil {
+				product.Images = nil
+			} else {
+				product.Images = images
+			}
+		} else {
+			product.Images = nil
+		}
+
+		if err := model.DB.Model(&product).Related(&product.Categories, "categories").Error; err != nil {
+			fmt.Println(err.Error())
+			SendErrJSON("error", ctx)
+			return
+		}
+
+		if product.HasProperty {
+			if err := model.DB.Model(&product).Related(&product.Properties).Error; err != nil {
+				fmt.Println(err.Error())
+				SendErrJSON("error", ctx)
+				return
+			}
+
+			for i := 0; i < len(product.Properties); i++ {
+				property := product.Properties[i]
+				if err := model.DB.Model(&property).Related(&property.PropertyValues).Error; err != nil {
+					fmt.Println(err.Error())
+					SendErrJSON("error", ctx)
+					return
+				}
+				product.Properties[i] = property
+			}
+
+			if err := model.DB.Model(&product).Related(&product.Inventories).Error; err != nil {
+				fmt.Println(err.Error())
+				SendErrJSON("error", ctx)
+				return
+			}
+
+			for i := 0; i < len(product.Inventories); i++ {
+				inventory := product.Inventories[i]
+				if err := model.DB.Model(&inventory).Related(&inventory.PropertyValues, "property_values").Error; err != nil {
+					fmt.Println(err.Error())
+					SendErrJSON("error", ctx)
+					return
+				}
+				product.Inventories[i] = inventory
+			}
+		}
+		cp := CartProduct{
+			Cart:    cart,
+			Product: product,
+		}
+
+		cartProducts = append(cartProducts, cp)
+	}
 
 	ctx.JSON(iris.StatusOK, iris.Map{
-		"errNo" : model.ErrorCode.SUCCESS,
-		"msg"   : "success",
-		"data"  : iris.Map{
-			"carts": carts,
+		"errNo": model.ErrorCode.SUCCESS,
+		"msg":   "success",
+		"data": iris.Map{
+			"carts": cartProducts,
 		},
 	})
 }
-
--- wexin/pages/cart/cart.js
+++ wexin/pages/cart/cart.js
@@ -3,11 +3,13 @@ var config   = require('../../config/config.js');
 Page({
     data: {
         appName: config.name,
+        totalMoney: 0,
         carts: []
     },
     onLoad: function() {
-      var self = this
+      var self = this;
       var app = getApp();
+      var totalMoney = 0;
       // todo 验证是否登陆
       console.log(app)
       wx.request({
@@ -22,36 +24,19 @@ Page({
         // request user carts
         success: function (res) {
           var carts = res.data.data.carts;
-          var cartsInfo = []
+          var totalMoney = 0;
           console.log("Get user carts", carts)
 
           // request cart product info
           for (var i = 0; i < carts.length; i++) {
-            var productId = carts[i].productId
-            var productCount = carts[i].count;
-            wx.request({
-              url: config.api.reqProductDetail.replace(':id', productId),
-              success: function (res) {
-                console.log("Cart product info response", res)
-                var product = res.data.data.product || null;
-                product.image.url = config.static.imageDomain + product.image.url;
-                product.price = product.price.toFixed(2);
-                var cartDetail = {}
-                cartDetail.id = productId;
-                cartDetail.count = productCount
-                cartDetail.imageUrl = product.image.url;
-                cartDetail.name = product.name;
-                cartDetail.price = product.price;
-                cartDetail.checked = true;
-                cartsInfo.push(cartDetail);
-
-                console.log("Carts Info: ", cartsInfo)
-                self.setData({
-                  carts: cartsInfo,
-                }); 
-            }
+            var cp = carts[i]
+            cp.product.image.url = config.static.imageDomain + cp.product.image.url;
+            totalMoney += cp.cart.count * cp.product.price;
+          }
+          self.setData({
+            carts: carts,
+            totalMoney: totalMoney,
           }); 
-        }
       }
     });
   }
